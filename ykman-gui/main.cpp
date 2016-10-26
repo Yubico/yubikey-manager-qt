4550704a852cab311a5997042d4a6659e7a95c50
Dag Heyman <dag@yubico.com>
2016-10-26 13:55:19 +0200

    Add app meta data

--- ykman-gui/main.cpp
+++ ykman-gui/main.cpp
@@ -8,18 +8,26 @@
 
 int main(int argc, char *argv[])
 {
+    #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    #endif
+
     // Only allow a single instance running.
     QtSingleApplication app(argc, argv);
     if (app.sendMessage("")) {
         return 0;
     }
 
-    QQmlApplicationEngine engine;
+    app.setOrganizationName("Yubico");
+    app.setApplicationName("YubiKey Manager");
+    app.setApplicationDisplayName("YubiKey Manager");
+    app.setApplicationVersion("0.2.0");
+
+    qputenv("PYTHONDONTWRITEBYTECODE", "1");
+
     app.setWindowIcon(QIcon("resources/icons/ykman.png"));
-    QString pythonNoBytecode = "PYTHONDONTWRITEBYTECODE=1";
-    putenv(pythonNoBytecode.toUtf8().data());
-    QString frameworks = "DYLD_LIBRARY_PATH=" + app.applicationDirPath() + "/../Frameworks";
-    putenv(frameworks.toUtf8().data());
+    app.setOrganizationName("Yubico");
+    QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
 
     // Wake up the root window on a message from new instance.
