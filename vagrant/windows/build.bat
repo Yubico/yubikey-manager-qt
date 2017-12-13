17ec0fb8019d14c2ada9d385e2050e31d149af45
Emil Lundberg <emil@yubico.com>
2017-12-13 17:18:57 +0100

    Adjust Windows build VM for building ykman-qt

--- vagrant/windows/build.bat
+++ vagrant/windows/build.bat
@@ -9,7 +9,8 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"\vcvarsall.bat x86
 REM Add python and Qt to PATH
 SET "PATH=%PATH%;C:\Python36\Scripts;C:\Qt\%QT_VERSION%\msvc2015\bin;C:\Qt\Tools\QtCreator\bin;"
 
-SET RELEASE_DIR=".\release"
+SET RELEASE_DIR=".\ykman-gui\release"
+SET CLI_RELEASE_DIR=".\ykman-cli\release"
 
 
 REM Clean build
@@ -19,17 +20,20 @@ git -C vendor\yubikey-manager clean -dfx
 mkdir "%RELEASE_DIR%"
 
 REM Build
-qmake
+qmake yubikey-manager-qt.pro
+qmake -o ykman-gui\Makefile ykman-gui\ykman-gui.pro
 
-rm -rf .\vendor\yubikey-manager\ykman\yubicommon
+REM Fix problem with symbolic link
+rm .\vendor\yubikey-manager\ykman\yubicommon
 xcopy /i /e .\vendor\yubikey-manager\vendor\yubicommon\yubicommon .\vendor\yubikey-manager\ykman\yubicommon
+jom
 
 jom
 
 REM Package
 
 wget "https://developers.yubico.com/libu2f-host/Releases/libu2f-host-%LIBU2F_HOST_VERSION%-win32.zip" -O "libu2f-host-%LIBU2F_HOST_VERSION%-win32.zip"
-7z x libu2f-host-%LIBU2F_HOST_VERSION%-win32.zip -o".\libu2f-host"
+7z x "libu2f-host-%LIBU2F_HOST_VERSION%-win32.zip" -o".\libu2f-host"
 powershell -Command "Copy-Item .\libu2f-host\bin\*.dll %RELEASE_DIR% -Force"
 
 wget "https://developers.yubico.com/yubikey-personalization/Releases/ykpers-%YKPERS_VERSION%-win32.zip" -O "ykpers-%YKPERS_VERSION%-win32.zip"
@@ -45,7 +49,7 @@ wget "https://www.python.org/ftp/python/%PY_VERSION%/python-%PY_VERSION%-embed-w
 7z x "python-%PY_VERSION%-embed-win32.zip" -o%RELEASE_DIR%
 
 REM Use Qt deployment tool on executable
-windeployqt "%RELEASE_DIR%"\yubioath-desktop.exe --qmldir=qml --no-translations --angle --release
+windeployqt "%RELEASE_DIR%"\ykman-gui.exe -qmldir=.\ykman-gui\qml --no-translations --angle --release
 
 REM Workaround:
 REM Manually add pyotherside plugin to release folder.
@@ -53,7 +57,7 @@ REM Should be handled by windeployqt, but doesn't seem to be when QML Settings a
 powershell -Command "Copy-Item C:\Qt\%QT_VERSION%\msvc2015\qml\io %RELEASE_DIR% -Recurse -Force"
 
 REM Add python dependencies to release folder
-powershell -Command "Copy-Item .\pymodules %RELEASE_DIR% -Recurse"
+powershell -Command "Copy-Item .\ykman-gui\pymodules %RELEASE_DIR% -Recurse"
 
 REM Remove .pyc files from release folder
 powershell -Command "Get-ChildItem -Path %RELEASE_DIR% -File -Include *.pyc -Recurse | Remove-Item -Force"
@@ -65,7 +69,13 @@ powershell -Command "Get-ChildItem -Path %RELEASE_DIR% -Include *.cpp -Recurse |
 REM Remove object files
 powershell -Command "Get-ChildItem -Path %RELEASE_DIR% -Include *.obj -Recurse | Remove-Item -Force"
 
-REM Remove vcruntime140.dll and sqllite3.dll from python-embed
-powershell -Command "Get-ChildItem -Path %RELEASE_DIR% -Include vcruntime140.dll -Recurse | Remove-Item -Force"
-powershell -Command "Get-ChildItem -Path %RELEASE_DIR% -Include sqlite3.dll -Recurse | Remove-Item -Force"
-powershell -Command "Get-ChildItem -Path %RELEASE_DIR% -Include _sqlite3.pyd -Recurse | Remove-Item -Force"
+REM Add CLI executable to release folder
+copy "%CLI_RELEASE_DIR%"\ykman.exe "%RELEASE_DIR%"
+
+REM Create zip for deployment
+7z a yubikey-manager-qt-dev-win.zip "%RELEASE_DIR%"\*
+
+REM Verify that the cli tool can run
+copy .\ykman-cli\test.py "%RELEASE_DIR%"
+cd "%RELEASE_DIR%"
+python test.py
--- vagrant/windows/release.bat
+++ vagrant/windows/release.bat
@@ -2,15 +2,16 @@ SET VERSION="%1"
 
 ECHO "Building release of version: %VERSION%"
 
-SET RELEASE_DIR=".\release"
+SET RELEASE_DIR=".\ykman-gui\release"
 
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"\vcvarsall.bat x86
 SET "PATH=%PATH%;C:\Program Files (x86)\NSIS"
 
 REM Download Appveyor build
-REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yubico-builds.s3-eu-west-1.amazonaws.com/yubioath-desktop/yubioath-desktop-yubioath-desktop-%VERSION%-win.zip', 'C:\Users\vagrant\Downloads\yubioath-desktop-%VERSION%-win.zip')"
-REM 7z x -o"%RELEASE_DIR%" C:\Users\vagrant\Downloads\yubioath-desktop-%VERSION%-win.zip
+REM powershell -Command "(New-Object Net.WebClient).DownloadFile('https://yubico-builds.s3-eu-west-1.amazonaws.com/yubikey-manager-qt/yubikey-manager-qt-yubikey-manager-qt-%VERSION%-win.zip', 'C:\Users\vagrant\Downloads\yubikey-manager-qt-%VERSION%-win.zip')"
+REM 7z x -o"%RELEASE_DIR%" C:\Users\vagrant\Downloads\yubikey-manager-qt-%VERSION%-win.zip
 
-signtool sign /fd SHA256 /t http://timestamp.verisign.com/scripts/timstamp.dll "%RELEASE_DIR%"\yubioath-desktop.exe
+signtool sign /fd SHA256 /t http://timestamp.verisign.com/scripts/timstamp.dll "%RELEASE_DIR%"\yubikey-manager-qt.exe
 makensis -D"VERSION=%VERSION%" resources\win\win-installer.nsi
-signtool sign /fd SHA256 /t http://timestamp.verisign.com/scripts/timstamp.dll "yubioath-desktop-%VERSION%-win.exe"
-gpg --detach-sign "yubioath-desktop-%VERSION%-win.exe"
+signtool sign /fd SHA256 /t http://timestamp.verisign.com/scripts/timstamp.dll "yubikey-manager-qt-%VERSION%-win.exe"
+gpg --detach-sign "yubikey-manager-qt-%VERSION%-win.exe"
