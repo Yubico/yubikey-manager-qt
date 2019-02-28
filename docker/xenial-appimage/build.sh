4df251e6d3fb1dd18bf332d3632abbaf94663abb
Dag Heyman <dag@yubico.com>
2019-02-28 15:22:58 +0100

    update versions in travis and appveyor

--- docker/xenial-appimage/build.sh
+++ docker/xenial-appimage/build.sh
@@ -11,9 +11,9 @@ git clone /sources/yubikey-manager-qt /yubikey-manager-qt
 
 mkdir -p /yubikey-manager-qt/appDir/usr
 eval "$(pyenv init -)"
-pyenv global 3.6.5
+pyenv global 3.6.8
 pip3 install -r /yubikey-manager-qt/requirements.txt
-cp -R /root/.pyenv/versions/3.6.5/* /yubikey-manager-qt/appDir/usr
+cp -R /root/.pyenv/versions/3.6.8/* /yubikey-manager-qt/appDir/usr
 dpkg -x /libykpers*.deb /yubikey-manager-qt/appDir/
 
 cd /yubikey-manager-qt
